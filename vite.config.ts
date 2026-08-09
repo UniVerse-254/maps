57e1f59534afa3cd9cfb702607441c64588e8a00
Yona Paul <ypaul6764@gmail.com>
2026-08-09 16:26:13 +0300

    final fix

--- vite.config.ts
+++ vite.config.ts
@@ -53,6 +53,12 @@ export default defineConfig({
             src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
             purpose: "maskable",
           },
         ],
